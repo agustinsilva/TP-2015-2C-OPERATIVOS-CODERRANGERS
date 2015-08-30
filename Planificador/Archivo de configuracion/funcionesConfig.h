2b5b731554808d5530117ea020a4eb8b9ee59770
dandrestorralba <dandrestorralba@gmail.com>
2015-08-30 17:41:30 -0300

    Archivo de configuracion para Planificador OK SIN ERRORES

---
+++ Planificador/Archivo de configuracion/funcionesConfig.h
@@ -0,0 +1,16 @@
+//*********************************************************************//
+//********************HEADERS-FUNCIONES-ARCH*********************//
+//*******************************************************************//
+
+
+#ifndef FUNCIONES_H_
+#define FUNCIONES_H_
+
+#include <commons/config.h>
+
+
+int conf_es_valida(t_config * configuracion);
+void* cargarArchivoDeConfiguracion();
+
+
+#endif /* FUNCIONES_H_ */
---
+++ Planificador/src/planificador.c
@@ -0,0 +1,12 @@
+/***********PLANIFICADOR************/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+
+	puts("Cargo archivo de configuracion de Planificador");
+	cargarArchivoDeConfiguracion();
+
+	return EXIT_SUCCESS;
+}
