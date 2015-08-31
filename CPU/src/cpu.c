72762a9b0ea632a2e3a751c260300541a7024ef8
mariana13193 <mariana13193@gmail.com>
2015-08-30 21:14:36 -0300

    Archivo de configuracion para proceso CPU

---
+++ CPU/src/cpu.c
@@ -0,0 +1,12 @@
+/***********CPU************/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+
+	puts("Cargo archivo de configuracion de CPU");
+	cargarArchivoDeConfiguracion();
+
+	return EXIT_SUCCESS;
+}
