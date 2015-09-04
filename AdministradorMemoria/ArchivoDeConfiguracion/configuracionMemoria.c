5a543ec6c320baba5e5e006185248ac40d9156a0
ElianaLS <eliana.lugueros@hotmail.com>
2015-09-04 11:57:39 -0300

    Levanto arch de configuración del Admin de Memoria desde el main con TAD correspondiente

--- AdministradorMemoria/ArchivoDeConfiguracion/configuracionMemoria.c
+++ AdministradorMemoria/ArchivoDeConfiguracion/configuracionMemoria.c
@@ -3,7 +3,7 @@
 
 #include "funcionesConfig.h"
 
-int conf_es_valida(t_config * configuracion)
+int32_t conf_es_valida(t_config * configuracion)
 {
 	// config_has_property Retorna true si key se encuentra en la configuracion.
 	return (config_has_property(configuracion, "PUERTO_ESCUCHA")
@@ -18,48 +18,41 @@ int conf_es_valida(t_config * configuracion)
 	);
 }
 
-int cargarArchivoDeConfiguracion()
+t_Memoria_Config* cargarArchivoDeConfiguracion()
 {
 	t_config * configuracion;
-	int puerto_escucha;
-	char* ip_swap;
-	int puerto_swap;
-	int maximo_marcos_por_proceso;
-	int cantidad_marcos;
-	int tamanio_marco;
-	int entradas_tlb;
-	int tlb_habilitada;
-	int retardo_memoria;
+	t_Memoria_Config* memoriaConfig = malloc(sizeof(t_Memoria_Config));
 
 	configuracion= config_create("ArchivoDeConfiguracion/configMemoria.txt");
 	
 	if (!conf_es_valida(configuracion)) //ver que el archivo config este completo
 	{
 		puts("Archivo de configuracion incompleto o invalido.\n");
-		return -2;
+		return NULL;
 	}
        
-	puerto_escucha = config_get_int_value(configuracion, "PUERTO_ESCUCHA");
-	ip_swap = config_get_string_value(configuracion, "IP_SWAP");
-	puerto_swap = config_get_int_value(configuracion, "PUERTO_SWAP");
-	maximo_marcos_por_proceso = config_get_int_value(configuracion, "MAXIMO_MARCOS_POR_PROCESO");
-	cantidad_marcos = config_get_int_value(configuracion, "CANTIDAD_MARCOS");
-	tamanio_marco = config_get_int_value(configuracion, "TAMANIO_MARCO");
-	entradas_tlb = config_get_int_value(configuracion, "ENTRADAS_TLB");
-	tlb_habilitada = config_get_int_value(configuracion, "TLB_HABILITADA");
-	retardo_memoria = config_get_int_value(configuracion, "RETARDO_MEMORIA");
+	memoriaConfig->puerto_escucha = config_get_int_value(configuracion, "PUERTO_ESCUCHA");
+	memoriaConfig->ip_swap = string_new();
+	memoriaConfig->ip_swap = config_get_string_value(configuracion, "IP_SWAP");
+	memoriaConfig->puerto_swap = config_get_int_value(configuracion, "PUERTO_SWAP");
+	memoriaConfig->maximo_marcos_por_proceso = config_get_int_value(configuracion, "MAXIMO_MARCOS_POR_PROCESO");
+	memoriaConfig->cantidad_marcos = config_get_int_value(configuracion, "CANTIDAD_MARCOS");
+	memoriaConfig->tamanio_marco = config_get_int_value(configuracion, "TAMANIO_MARCO");
+	memoriaConfig->entradas_tlb = config_get_int_value(configuracion, "ENTRADAS_TLB");
+	memoriaConfig->tlb_habilitada = config_get_int_value(configuracion, "TLB_HABILITADA");
+	memoriaConfig->retardo_memoria = config_get_int_value(configuracion, "RETARDO_MEMORIA");
 	
-	printf("PUERTO_ESCUCHA: %d\n", puerto_escucha);
-	printf("IP_SWAP: %s\n", ip_swap);
-	printf("PUERTO_SWAP: %d\n", puerto_swap);
-	printf("MAXIMO_MARCOS_POR_PROCESO: %d\n", maximo_marcos_por_proceso);
-	printf("CANTIDAD_MARCOS: %d\n", cantidad_marcos);
-	printf("TAMANIO_MARCO: %d\n", tamanio_marco);
-	printf("ENTRADAS_TLB: %d\n", entradas_tlb);
-	printf("TLB_HABILITADA: %d\n", tlb_habilitada);
-	printf("RETARDO_MEMORIA: %d\n", retardo_memoria);
+	printf("PUERTO_ESCUCHA: %d\n", memoriaConfig->puerto_escucha);
+	printf("IP_SWAP: %s\n", memoriaConfig->ip_swap);
+	printf("PUERTO_SWAP: %d\n", memoriaConfig->puerto_swap);
+	printf("MAXIMO_MARCOS_POR_PROCESO: %d\n", memoriaConfig->maximo_marcos_por_proceso);
+	printf("CANTIDAD_MARCOS: %d\n", memoriaConfig->cantidad_marcos);
+	printf("TAMANIO_MARCO: %d\n", memoriaConfig->tamanio_marco);
+	printf("ENTRADAS_TLB: %d\n", memoriaConfig->entradas_tlb);
+	printf("TLB_HABILITADA: %d\n", memoriaConfig->tlb_habilitada);
+	printf("RETARDO_MEMORIA: %d\n", memoriaConfig->retardo_memoria);
 
-	return 0;
+	return memoriaConfig;
 }
 
 //***************************FIN********************************//
--- AdministradorMemoria/src/administradorMemoria.c
+++ AdministradorMemoria/src/administradorMemoria.c
@@ -6,7 +6,12 @@
 int main(void) {
 
 	puts("Cargo archivo de configuracion de Administrador Memoria");
-	cargarArchivoDeConfiguracion();
+	t_Memoria_Config* config = cargarArchivoDeConfiguracion();
+
+	if(config==NULL){
+		printf("No se pudo cargar el archivo de configuración\n"); //se debería loguear, no printear
+		return EXIT_FAILURE;
+	}
 
 	return EXIT_SUCCESS;
 }
