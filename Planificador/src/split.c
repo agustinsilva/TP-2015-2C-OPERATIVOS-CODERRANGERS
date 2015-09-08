f48249d4603af69d79c36b74c9026eac8e9a699b
dandrestorralba <dandrestorralba@gmail.com>
2015-09-07 23:42:43 -0300

    Separando en otra funcion para reutilizar

--- Planificador/src/split.c
+++ Planificador/src/split.c
@@ -3,27 +3,50 @@
  *
  *  Created on: 7/9/2015
  *      Author: utnso
- */
+/*EL MENSAJE QUE SE ENVIA A TRAVES DEL SOCKET ES lista[1] que es el segundo argumento*/
+
 
 /*
 #include <commons/string.h>
 #include <commons/collections/list.h>
 #include <stdio.h>
 
-int main () {
+int validarArgumentosCorrer(char comando,char*comandoEsperado);
+
+int main ()
+{
 
 	char comando[50];
 	char** lista;
-	char*comandoClave="correr";
+	char*comandoCorrer="correr";
 
 	printf ("Ingrese un comando\n");
 	scanf("%s",comando);
 
+	if(validarArgumentosCorrer(comando,comandoCorrer)==1)
+	{
+		lista = string_split(comando," ");
+		printf("Se ejecutará el programa\n");
+
+	}
+		else printf("Incorrecto\n");
+
+return 0;
+}
+
+*/
+/*************************FUNCION*********************/
+
+/*
+int validarArgumentosCorrer(char comando,char*comandoEsperado)
+{
+	char** lista;
 	lista = string_split(comando," ");
 
-	if (string_equals_ignore_case(lista[0], comandoClave)) 
-		 printf("Se ejecutará el programa\n");
-		else printf("Comando incorrecto\n");
+	if (string_equals_ignore_case(lista[0], comandoEsperado))
+		{
+			if ( (sizeof(lista)/sizeof(char*)) == 2 ) return 1;
+		}
 
 return 0;
 }
