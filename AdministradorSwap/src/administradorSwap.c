0f97b29b759ef9495f8caa650b699a554b974ef9
agustinsilva <agustin.silva@live.com>
2015-09-20 17:02:30 -0300

    Update administradorSwap.c
    Saco el comment del iniciar servidor

--- AdministradorSwap/src/administradorSwap.c
+++ AdministradorSwap/src/administradorSwap.c
@@ -10,7 +10,7 @@ int main(void) {
 	cargarArchivoDeConfiguracion();
 	printf("Creando particion\n");
 	inicializarParticion();
-	//iniciarServidor();
+	iniciarServidor();
 	printf("Finaliza Administrador de Swap\n");
 
 	eliminarParticion();
