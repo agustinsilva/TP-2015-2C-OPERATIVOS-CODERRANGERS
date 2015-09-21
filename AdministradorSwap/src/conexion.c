e6cfa7947a2c7bc72e3a0c480d0913ec97ff74af
Agustin Silva <agustin.silva@live.com>
2015-09-21 00:59:07 -0300

    Manejo de mensajes con memoria falta probar con integracion

--- AdministradorSwap/src/conexion.c
+++ AdministradorSwap/src/conexion.c
@@ -1,5 +1,7 @@
 #include "administradorSwap.h"
 
+
+//deprecado
 char* recibirMensaje(sock_t* socket){
 
 	/*recibe la cantidad de bytes que va a tener el mensaje*/
@@ -22,7 +24,8 @@ int32_t enviarMensaje(sock_t* socket, char* mensaje){
 	int32_t status = send(socket->fd, &longitud, sizeof(int32_t),0);
 
 	/*chequea envío*/
-	if(!status){
+	if(!status)
+	{
 		printf("No se envió la cantidad de bytes a enviar luego\n");
 		return status;
 	}
@@ -33,24 +36,83 @@ int32_t enviarMensaje(sock_t* socket, char* mensaje){
 
 void iniciarServidor()
 {
+	uint32_t cabecera;
+	uint32_t status;
+	t_mensaje* detalle;
+	bool resultado;
+	char* respuesta;
 	sock_t* socketServerSwap = create_server_socket(configuracion->puerto_escucha);
 	listen_connections(socketServerSwap);
 	printf("Escucha conexiones \n");
-	sock_t* socketCliente = accept_connection(socketServerSwap);
-	printf("Conexión: %d\n", socketCliente->fd);
-	char* mensaje = recibirMensaje(socketCliente);
-	printf("Mensaje de Admin memoria: %s\n", mensaje);
-	free(mensaje);
-	//envia mensaje
-	char* respuesta = "Hola Memoria, un gusto.";
-	int32_t status = enviarMensaje(socketCliente,respuesta);
-	//chequea envío
-	if(!status)
+	sock_t* socketMemoria = accept_connection(socketServerSwap);
+	printf("Conexión: %d\n", socketMemoria->fd);
+	while(1)
 	{
-		printf("No se envió el mensaje al swap\n");
+		cabecera = deserializarEnteroSinSigno(socketMemoria);
+		detalle = deserializarDetalle(socketMemoria, cabecera);
+
+		switch (cabecera)
+		{
+			case INICIAR:
+				resultado = asignarProceso(detalle);
+				status = send(socketMemoria->fd, &resultado, sizeof(bool),0);
+				/*chequea envío*/
+				if(!status)
+				{
+					printf("No se envió la cantidad de bytes a enviar luego\n");
+				}
+				break;
+			case FINALIZAR:
+				liberarEspacio(detalle->PID);
+				break;
+			case LEER:
+				respuesta = buscarPagina(detalle->PID, detalle->ubicacion);
+				enviarMensaje(socketMemoria, respuesta);
+				break;
+			case ESCRIBIR:
+				break;
+			default:
+				// Por si acaso
+				break;
+		}
 	}
-	else
+	clean_socket(socketServerSwap);
+	clean_socket(socketMemoria);
+}
+
+
+uint32_t deserializarEnteroSinSigno(sock_t* socket)
+{
+	uint32_t enteroSinSigno;
+	recv(socket->fd, &enteroSinSigno, sizeof(uint32_t), 0);
+	return enteroSinSigno;
+}
+
+t_mensaje* deserializarDetalle(sock_t* socket, uint32_t cabecera)
+{
+	t_mensaje* detalle;
+	switch (cabecera)
 	{
-		printf("Se envió a Memoria: %s\n", respuesta);
+		case INICIAR:
+			detalle = malloc(sizeof(uint32_t)*2);
+			detalle->PID = deserializarEnteroSinSigno(socket);
+			detalle->paginas = deserializarEnteroSinSigno(socket);
+			break;
+		case FINALIZAR:
+			detalle = malloc(sizeof(uint32_t));
+			detalle->PID = deserializarEnteroSinSigno(socket);
+			break;
+		case LEER:
+			detalle = malloc(sizeof(uint32_t)*2);
+			detalle->PID = deserializarEnteroSinSigno(socket);
+			detalle->ubicacion = deserializarEnteroSinSigno(socket);
+			break;
+		case ESCRIBIR:
+			break;
+		default:
+			// Por si acaso
+			break;
 	}
+
+	return detalle;
 }
