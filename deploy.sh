b14d07dec727d8d242c8fe42284f0e781165bba9
Agustin Silva <agustin.silva@live.com>
2015-11-27 18:37:51 -0300

    Archivo para deploy

---
+++ deploy.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+git clone https://github.com/sisoputnfrba/so-commons-library.git
+cd so-commons-library
+sudo make install
+cd ..
+git clone https://github.com/sisoputnfrba/tp-2015-2c-coderrangers.git
+cd tp-2015-2c-coderrangers/sockets/Instalador
+sudo make install
+cd ..
+cd ..
