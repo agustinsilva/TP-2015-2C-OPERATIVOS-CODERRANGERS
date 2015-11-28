e0fa2ace4a92063449a500a6fffd651e0b390ef1
Agustin Silva <agustin.silva@live.com>
2015-11-27 22:43:51 -0300

    Modificacion en deploy para no descargar repo

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
+cd ..
 git clone https://github.com/sisoputnfrba/so-commons-library.git
 cd so-commons-library
 sudo make install
 cd ..
-git clone https://github.com/sisoputnfrba/tp-2015-2c-coderrangers.git
 cd tp-2015-2c-coderrangers/sockets/Instalador
 sudo make install
 cd ..
 cd ..
+sudo make all
